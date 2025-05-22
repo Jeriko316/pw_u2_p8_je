082f46dd2d8133b648b22ecdd0ab0365f70a05d4
Jeriko316 <jkenriquez@hotmail.com>
2025-05-21 19:45:49 -0500

    taller16

--- proyecto1/src/App.vue
+++ proyecto1/src/App.vue
@@ -2,33 +2,32 @@
   <!--<HolaMundoView/> -->
   <!--<ContadorView/>-->
   <!--<EstudianteView />-->
-  <EstudianteTablaView />
+  <!--<EstudianteTablaView />-->
+  <PreguntaView/>
+  
 </template>
 
 <script>
-import HolaMundoView from "@/views/HolaMundoView.vue";
+/*import HolaMundoView from "@/views/HolaMundoView.vue";
 import ContadorView from "@/views/ContadorView.vue";
 import EstudianteView from "@/views/EstudianteView.vue";
-import EstudianteTablaView from "@/views/EstudianteTablaView.vue";
+import EstudianteTablaView from "@/views/EstudianteTablaView.vue";*/
+import PreguntaView from "@/views/PreguntaView.vue";
 
 export default {
   name: "App",
   components: {
-    HolaMundoView,
+  /*  HolaMundoView,
     ContadorView,
     EstudianteView,
-    EstudianteTablaView,
+    EstudianteTablaView,*/
+    PreguntaView,
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ 
 }
 </style>
